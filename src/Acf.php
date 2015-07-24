37f4787e815012cfeb4d705e8ee8c0d14d39643c
Francois Faubert <francois.faubert@iprospect.com>
2015-07-24 11:31:46 -0400

    fixed plugin autoloading

--- src/Acf.php
+++ src/Acf.php
@@ -8,14 +8,22 @@ class Acf {
 
     const FOLDER_NAME = 'acf';
 
-    public function load()
+    public function register()
+    {
+        if (function_exists("get_field")) {
+            add_filter('acf/settings/save_json', array($this, "save"));
+            add_filter('acf/settings/load_json', array($this, "load"));
+        }
+    }
+
+    public function load($paths)
     {
         unset($paths[0]);
         $paths[] = \Strata\Strata::getConfigurationPath() . self::FOLDER_NAME;
         return $paths;
     }
 
-    public function save()
+    public function save($paths)
     {
         $path = Strata::getConfigurationPath() . self::FOLDER_NAME;
         if (!is_dir($path)) {
--- src/PluginInitializer.php
+++ src/PluginInitializer.php
@@ -6,11 +6,7 @@ class PluginInitializer extends \Strata\Plugin\Plugin {
 
     public static function initialize()
     {
-        if (function_exists("get_field")) {
-            $acf = new Acf();
-            add_filter('acf/settings/save_json', array($acf, "save"));
-            add_filter('acf/settings/load_json', array($acf, "load"));
-        }
+        add_action('plugins_loaded', array(new Acf(), "register"));
     }
 
 }
